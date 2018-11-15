264576a87cedbf64cd9f7c0762796e22d45306e8
syarafina <farahiyahsyarafina@gmail.com>
2018-11-15 13:48:26 +0700

    add function to get category link

--- final.py
+++ final.py
@@ -1,37 +1,48 @@
+from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import TimeoutException
 import urllib.request
 from bs4 import BeautifulSoup
 from selenium import webdriver
-import time
 import pandas as pd
-from selenium.webdriver.chrome.options import Options
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import WebDriverException
+import time
 
-urlpage = 'https://www.tokopedia.com/p/makanan-minuman/biskuit-kue'
+startUrl = 'https://www.tokopedia.com/p/makanan-minuman/'
 
-#headless browser configuration
+'''Headless browser configuration'''
 options = Options()
 options.headless = True
-driver = webdriver.Chrome(options=options, executable_path = '/usr/local/bin/chromedriver/')
+driver = webdriver.Chrome(
+    options=options, executable_path='/usr/local/bin/chromedriver/')
 window_setting = 'window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;'
-
 # xpath only for promoted items
 xpath_promoted_product_name = '//div[@id="promo-new"]//span[@class="detail__name"]'
 xpath_promoted_link_shop = '//div[@id="promo-new"]//a[@class="muted"]'
 
 data = []
+xpath_item_array = []
+xpath_link_array = []
 xpath_list_product_content_dir = []
 xpath_list_link_shop_content_dir = []
 listWebElement_product = []
 listWebElement_link = []
+url = []
 
-#get the web page
-def start_browser(urlpage):
+def getAllCategoryLink():
+    print("Now getting all category link \n")
+    a = driver.find_elements_by_xpath(
+        '//div[@id="DeptIntermedController"]//div[@class="title ta-center"]//a')
+    for i in range(len(a)):
+        url.append(a[i].get_attribute("href"))
+
+'''start the browser and get the web page'''
+def getPage(urlpage):
+    print("Now working on: " + str(urlpage))
     driver.get(urlpage)
     driver.execute_script(window_setting)
     time.sleep(30)
-    print(urlpage)
 
+'''Get WebElement from xpath'''
 def get_data_list(xpath_product_name, xpath_link_shop):
     try:
         result_link_shop = driver.find_elements_by_xpath(xpath_link_shop)
@@ -47,6 +58,20 @@ def get_data_list(xpath_product_name, xpath_link_shop):
             listWebElement_link.append(result_link_shop)
             store_data_to_array(result_product_name, result_link_shop)
 
+'''Get Xpath non promo'''
+def get_list_data_content_dir():
+    try:
+        for index in range(1, 100):
+            xpath_product_name = '//*[@id="content-directory"]/div[' + \
+                str(index) + ']/div[2]/a[2]/div'
+            xpath_link_shop = '//*[@id="content-directory"]/div[' + \
+                str(index) + ']/div[3]/div/div[1]/div/div/div/a'
+            xpath_list_product_content_dir.append(xpath_product_name)
+            xpath_list_link_shop_content_dir.append(xpath_link_shop)
+    except(NoSuchElementException):
+        pass
+
+'''Store data to array'''
 def store_data_to_array(result_product_name, result_link_shop):
     try:
         for item_list in range(len(result_product_name)):
@@ -55,16 +80,6 @@ def store_data_to_array(result_product_name, result_link_shop):
             row = {"link_shop": link_shop, "item_name": item_name}
             data.append(row)
     except(AttributeError):
-        pass
-    
-def get_list_data_content_dir():
-    try:
-        for i in range(1,100):
-            xpath_product_name = '//*[@id="content-directory"]/div[' +str(i)+ ']/div[2]/a[2]/div'
-            xpath_link_shop = '//*[@id="content-directory"]/div[' +str(i)+ ']/div[3]/div/div[1]/div/div/div/a'
-            xpath_list_product_content_dir.append(xpath_product_name)
-            xpath_list_link_shop_content_dir.append(xpath_link_shop)
-    except(NoSuchElementException):
         pass
 
 if __name__ == "__main__":    
